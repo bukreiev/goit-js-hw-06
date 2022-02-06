371e6b951a2156988f2ca1227e900e1c56830abb
bukreiev <bukreiev@ukr.net>
2022-02-06 21:13:20 +0200

    new

---
+++ js/task-08.js
@@ -0,0 +1,19 @@
+
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Bсе поля должны быть заполнены');
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
